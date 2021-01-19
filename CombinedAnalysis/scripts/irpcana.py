22571dafb7fefc20d4d9f0cf52f4223376715dc5
mirabitl <laurent.mirabito@cern.ch>
2021-01-19 17:53:44 +0100

    Additional plots

--- CombinedAnalysis/scripts/irpcana.py
+++ CombinedAnalysis/scripts/irpcana.py
@@ -479,28 +479,63 @@ class analyse:
         return result
     def pltres(self):
         c=TCanvas("c","IRPC studies %d" % self.run ,545,345);
+        c.GetFrame().SetBorderSize( 12 )
+        gStyle.SetOptStat(0)
         self.f0.cd("/FEB/Chamber1/Strips/");
         
         XY = self.f0.Get("/FEB/Chamber1/Strips/XY");									       
         XY.SetTitle("Strip hits position in the time window %s" % self.comment);
         XY.GetXaxis().SetRangeUser(0.,35.);
+        XY.GetYaxis().SetRangeUser(0.,200.);
         XY.GetYaxis().SetTitle("Y_{Strip} (cm)");
         XY.GetXaxis().SetTitle("X_{Strip} (cm)");
         XY.Draw();
+        t8 =TText(8.,120.,"EM888");
+        t8.SetTextAlign(22);
+        t8.SetTextColor(2);
+        t8.SetTextFont(43);
+        t8.SetTextSize(12);
+        t8.SetTextAngle(90);
+        t8.Draw("SAME");
+        t4 =TText(22.,120.,"FR4");
+        t4.SetTextAlign(22);
+        t4.SetTextColor(3);
+        t4.SetTextFont(43);
+        t4.SetTextSize(12);
+        t4.SetTextAngle(90);
+        t4.Draw("SAME");
+        l=TLine(0.,150.0,35.,150.);
+        l.SetLineColor(4);
+        l.SetLineWidth(2);
+        l.Draw("SAME");
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_StripXYAll.png" % self.run);
+        
         XYMin = self.f0.Get("/FEB/Chamber1/Strips/XYMin");									       
         XYMin.SetTitle("Strip hits position nearest to the extrapolation %s" % self.comment);
         XYMin.GetXaxis().SetRangeUser(0.,35.);
+        XYMin.GetYaxis().SetRangeUser(0.,200.);
         XYMin.GetYaxis().SetTitle("Y_{Strip} (cm)");
         XYMin.GetXaxis().SetTitle("X_{Strip} (cm)");
         XYMin.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_StripXYMin.png" % self.run);
+        
         XYSel = self.f0.Get("/FEB/Chamber1/Strips/XYSel");
         XYSel.SetTitle("Strip hit associated %s" % self.comment);
         XYSel.GetXaxis().SetRangeUser(0.,35.);
+        XYSel.GetYaxis().SetRangeUser(0.,200.);
         XYSel.GetYaxis().SetTitle("Y_{Strip} (cm)");
         XYSel.GetXaxis().SetTitle("X_{Strip} (cm)");
         XYSel.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
+
         c.SaveAs("Run%d_StripXYSelected.png" % self.run);
         
         DXDY = self.f0.Get("/FEB/Chamber1/Strips/DXDY");
@@ -508,43 +543,48 @@ class analyse:
         DXDY.GetYaxis().SetTitle("#delta Y_{Strip} (cm)");
         DXDY.GetXaxis().SetTitle("#delta X_{Strip} (cm)");
         DXDY.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
+
         c.SaveAs("Run%d_StripDXDYSelected.png" % self.run);
         
-        AbsoluteTimeDelta= self.f0.Get("/FEB/Chamber1/Strips/AbsoluteTimeDelta");
-        AbsoluteTimeDelta.SetTitle("Other hits, Absolute Time distance to the selected hit time %s" % self.comment);
-        AbsoluteTimeDelta.GetXaxis().SetTitle("#delta T_{Absolute} (ns)");
-        AbsoluteTimeDelta.GetYaxis().SetTitle("Number of hits");
-        AbsoluteTimeDelta.GetXaxis().SetRangeUser(-300.,300.);
-        AbsoluteTimeDelta.Draw();
-        c.SaveAs("Run%d_StripAbsoluteDeltaT.png" % self.run);
-        AbsoluteTimeDelta.GetXaxis().SetRangeUser(-30.,130.);
-        AbsoluteTimeDelta.Draw();
-        c.SaveAs("Run%d_StripAbsoluteDeltaTZoomed.png" % self.run);
-
         self.f0.cd("/FEB/Chamber1/ClusterNew/");
         
         XYC = self.f0.Get("/FEB/Chamber1/ClusterNew/XY");									       
         XYC.SetTitle("Cluster position in the time window %s" % self.comment);
         XYC.GetXaxis().SetRangeUser(0.,35.);
+        XYC.GetYaxis().SetRangeUser(0.,200.);
         XYC.GetYaxis().SetTitle("Y_{Strip} (cm)");
         XYC.GetXaxis().SetTitle("X_{Strip} (cm)");
         XYC.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_ClusterXYAll.png" % self.run);
         
         XYMinC = self.f0.Get("/FEB/Chamber1/ClusterNew/XYMax");									       
         XYMinC.SetTitle("Cluster position nearest to the extrapolation %s" % self.comment);
         XYMinC.GetXaxis().SetRangeUser(0.,35.);
+        XYMinC.GetYaxis().SetRangeUser(0.,200.);
         XYMinC.GetYaxis().SetTitle("Y_{Strip} (cm)");
         XYMinC.GetXaxis().SetTitle("X_{Strip} (cm)");
         XYMinC.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_ClusterXYMin.png" % self.run);
         
         XYSelC = self.f0.Get("/FEB/Chamber1/ClusterNew/XYSel");
         XYSelC.SetTitle("Cluster associated %s" % self.comment);
         XYSelC.GetXaxis().SetRangeUser(0.,35.);
+        XYSelC.GetYaxis().SetRangeUser(0.,200.);
         XYSelC.GetYaxis().SetTitle("Y_{Strip} (cm)");
         XYSelC.GetXaxis().SetTitle("X_{Strip} (cm)");
         XYSelC.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_ClusterXYSelected.png" % self.run);
         
         DXDYC = self.f0.Get("/FEB/Chamber1/ClusterNew/DIST");
@@ -552,6 +592,9 @@ class analyse:
         DXDYC.GetYaxis().SetTitle("#delta Y_{Strip} (cm)");
         DXDYC.GetXaxis().SetTitle("#delta X_{Strip} (cm)");
         DXDYC.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_ClusterDXDYSelected.png" % self.run);
         
         hnclus= self.f0.Get("/FEB/Chamber1/ClusterNew/Clusters");
@@ -559,6 +602,9 @@ class analyse:
         hnclus.GetXaxis().SetTitle("Number of clusters reconstructed in the event");
         hnclus.GetYaxis().SetTitle("Number of events");
         hnclus.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_ClusterNclus.png" % self.run);
     
         cluss= self.f0.Get("/FEB/Chamber1/ClusterNew/ClusterSize");
@@ -566,6 +612,9 @@ class analyse:
         cluss.GetXaxis().SetTitle("Number of strip hits in cluster");
         cluss.GetYaxis().SetTitle("Number of events");
         cluss.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_ClusterClusterSize.png" % self.run);
     
         cluss1= self.f0.Get("/FEB/Chamber1/ClusterNew/ClusterSize1");
@@ -573,8 +622,185 @@ class analyse:
         cluss1.GetXaxis().SetTitle("Number of strip hits in cluster");
         cluss1.GetYaxis().SetTitle("Number of events");
         cluss1.Draw();
+        c.Modified()
+        c.Update()
+        val=raw_input()
         c.SaveAs("Run%d_ClusterClusterSizeAssociated.png" % self.run);
- 
 
 
+        c.Clear()
+        c.Divide(2,2)
+
+        c.cd(1)
+        hrsel=self.f0.Get("/FEB/Clustering1D/HRCount")
+        hrsel.GetXaxis().SetTitle("Number of strip")
+        hrsel.GetYaxis().SetTitle("Number of events")
+        lrsel=self.f0.Get("/FEB/Clustering1D/LRCount")
+        lrsel.GetXaxis().SetTitle("Number of strip")
+        lrsel.GetYaxis().SetTitle("Number of events")
+        hrmis=self.f0.Get("/Missed/Clustering1D/HRCount")
+        hrmis.GetXaxis().SetTitle("Number of strip")
+        hrmis.GetYaxis().SetTitle("Number of events")
+        lrmis=self.f0.Get("/Missed/Clustering1D/LRCount")
+        lrmis.GetXaxis().SetTitle("Number of strip")
+        lrmis.GetYaxis().SetTitle("Number of events")
+        gStyle.SetOptStat(110)
+        gStyle.SetOptTitle(0);
+        hrsel.Draw()
+        c.cd(3)
+        hrmis.Draw()
+        c.cd(2)
+        lrsel.Draw()
+        c.cd(4)
+        lrmis.Draw()
+        c.Modified()
+        c.Update()
+        val=raw_input()
+        c.SaveAs("Run%d_MissedPattern.png" % self.run);
+        
+
+    def setTDRStyle(self):
+
+        tdrStyle = TStyle("tdrStyle","Style for P-TDR");
+
+        # For the canvas:
+        tdrStyle.SetCanvasBorderMode(0);
+        tdrStyle.SetCanvasColor(kWhite);
+        tdrStyle.SetCanvasDefH(600); #Height of canvas
+        tdrStyle.SetCanvasDefW(600); #Width of canvas
+        tdrStyle.SetCanvasDefX(0);   #POsition on screen
+        tdrStyle.SetCanvasDefY(0);
+
+        # For the Pad:
+        tdrStyle.SetPadBorderMode(0);
+        # tdrStyle.SetPadBorderSize(Width_t size = 1);
+        tdrStyle.SetPadColor(kWhite);
+        tdrStyle.SetPadGridX(False);
+        tdrStyle.SetPadGridY(False);
+        tdrStyle.SetGridColor(0);
+        tdrStyle.SetGridStyle(3);
+        tdrStyle.SetGridWidth(1);
+
+        # For the frame:
+        tdrStyle.SetFrameBorderMode(0);
+        tdrStyle.SetFrameBorderSize(1);
+        tdrStyle.SetFrameFillColor(0);
+        tdrStyle.SetFrameFillStyle(0);
+        tdrStyle.SetFrameLineColor(1);
+        tdrStyle.SetFrameLineStyle(1);
+        tdrStyle.SetFrameLineWidth(1);
+        
+        # For the histo:
+        # tdrStyle.SetHistFillColor(1);
+        # tdrStyle.SetHistFillStyle(0);
+        tdrStyle.SetHistLineColor(1);
+        tdrStyle.SetHistLineStyle(0);
+        tdrStyle.SetHistLineWidth(1);
+        # tdrStyle.SetLegoInnerR(Float_t rad = 0.5);
+        # tdrStyle.SetNumberContours(Int_t number = 20);
+
+        tdrStyle.SetEndErrorSize(2);
+        # tdrStyle.SetErrorMarker(20);
+        #tdrStyle.SetErrorX(0.);
+        
+        tdrStyle.SetMarkerStyle(20);
+  
+        #For the fit/function:
+        tdrStyle.SetOptFit(1);
+        tdrStyle.SetFitFormat("5.4g");
+        tdrStyle.SetFuncColor(2);
+        tdrStyle.SetFuncStyle(1);
+        tdrStyle.SetFuncWidth(1);
+    
+        #For the date:
+        tdrStyle.SetOptDate(0);
+        # tdrStyle.SetDateX(Float_t x = 0.01);
+        # tdrStyle.SetDateY(Float_t y = 0.01);
+        
+        # For the statistics box:
+        tdrStyle.SetOptFile(0);
+        tdrStyle.SetOptStat(0); # To display the mean and RMS:   SetOptStat("mr");
+        tdrStyle.SetStatColor(kWhite);
+        tdrStyle.SetStatFont(42);
+        tdrStyle.SetStatFontSize(0.025);
+        tdrStyle.SetStatTextColor(1);
+        tdrStyle.SetStatFormat("6.4g");
+        tdrStyle.SetStatBorderSize(1);
+        tdrStyle.SetStatH(0.1);
+        tdrStyle.SetStatW(0.15);
+        # tdrStyle.SetStatStyle(Style_t style = 1001);
+        # tdrStyle.SetStatX(Float_t x = 0);
+        # tdrStyle.SetStatY(Float_t y = 0);
+
+        # Margins:
+        tdrStyle.SetPadTopMargin(0.10);
+        tdrStyle.SetPadBottomMargin(0.13);
+        tdrStyle.SetPadLeftMargin(0.16);
+        tdrStyle.SetPadRightMargin(0.02);
+
+        # For the Global title:
+
+        tdrStyle.SetOptTitle(1);
+        tdrStyle.SetTitleFont(42);
+        tdrStyle.SetTitleColor(1);
+        tdrStyle.SetTitleTextColor(1);
+        tdrStyle.SetTitleFillColor(10);
+        tdrStyle.SetTitleFontSize(0.05);
+        tdrStyle.SetTitleH(0); # Set the height of the title box
+        tdrStyle.SetTitleW(0); # Set the width of the title box
+        tdrStyle.SetTitleX(0); # Set the position of the title box
+        tdrStyle.SetTitleY(0.985); # Set the position of the title box
+        tdrStyle.SetTitleStyle(1001);
+        tdrStyle.SetTitleBorderSize(2);
+
+        # For the axis titles:
+        
+        tdrStyle.SetTitleColor(1, "XYZ");
+        tdrStyle.SetTitleFont(42, "XYZ");
+        tdrStyle.SetTitleSize(0.06, "XYZ");
+        # tdrStyle.SetTitleXSize(Float_t size = 0.02); # Another way to set the size?
+        # tdrStyle.SetTitleYSize(Float_t size = 0.02);
+        tdrStyle.SetTitleXOffset(0.9);
+        tdrStyle.SetTitleYOffset(1.25);
+        # tdrStyle.SetTitleOffset(1.1, "Y"); # Another way to set the Offset
+
+        # For the axis labels:
+
+        tdrStyle.SetLabelColor(1, "XYZ");
+        tdrStyle.SetLabelFont(42, "XYZ");
+        tdrStyle.SetLabelOffset(0.007, "XYZ");
+        tdrStyle.SetLabelSize(0.05, "XYZ");
+
+        # For the axis:
+
+        tdrStyle.SetAxisColor(1, "XYZ");
+        tdrStyle.SetStripDecimals(True);
+        tdrStyle.SetTickLength(0.03, "XYZ");
+        tdrStyle.SetNdivisions(510, "XYZ");
+        tdrStyle.SetPadTickX(1);  # To get tick marks on the opposite side of the frame
+        tdrStyle.SetPadTickY(1);
+        
+        # Change for log plots:
+        tdrStyle.SetOptLogx(0);
+        tdrStyle.SetOptLogy(0);
+        tdrStyle.SetOptLogz(0);
+
+        # Postscript options:
+        tdrStyle.SetPaperSize(20.,20.);
+        # tdrStyle.SetLineScalePS(Float_t scale = 3);
+        # tdrStyle.SetLineStyleString(Int_t i, const char* text);
+        # tdrStyle.SetHeaderPS(const char* header);
+        # tdrStyle.SetTitlePS(const char* pstitle);
+        
+        # tdrStyle.SetBarOffset(Float_t baroff = 0.5);
+        # tdrStyle.SetBarWidth(Float_t barwidth = 0.5);
+        # tdrStyle.SetPaintTextFormat(const char* format = "g");
+        # tdrStyle.SetPalette(Int_t ncolors = 0, Int_t* colors = 0);
+        # tdrStyle.SetTimeOffset(Double_t toffset);
+        # tdrStyle.SetHistMinimumZero(kTRUE);
+        
+        tdrStyle.SetHatchesLineWidth(5);
+        tdrStyle.SetHatchesSpacing(0.05);
+
+        tdrStyle.cd();
 
